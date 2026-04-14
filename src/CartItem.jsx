71dc5d8fabe222ebd1846bc25e7b1751c7809a2e
sajith-dev <148557049+sajith-dev@users.noreply.github.com>
2026-04-14 14:43:51 -0400

    Update CartItem

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeItem, updateQuantity } from './CartSlice';
+import { removeItem, updateQuantity, addItem } from './CartSlice';
 import './CartItem.css';
 
 const CartItem = ({ onContinueShopping }) => {
@@ -51,6 +51,10 @@ const CartItem = ({ onContinueShopping }) => {
   const handleRemove = (item) => {
     dispatch(removeItem(item.name));
   };
+
+  const handleAddItem = (item) => {
+    dispatch(addItem(item));
+  }
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
